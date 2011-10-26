b20e467eeef46e4c435fe3e58bab5d704c47da4c
Terence Ponce <terenceponce@gmail.com>
2011-10-26 19:08:53 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePOS::Application.config.secret_token = '03388e57d10c8a28540c51a14080034ea73f78b086e5fa121d0a3fb027e23b84272b5235bf690667c7417590bb06f0ac8760a7a6fce4058509c8454f01c1e289'
